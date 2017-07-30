8e1b1cc82188637744ae4445de2059018fdc5a0a
pgolding <goldingp@gmail.com>
2017-07-30 10:04:44 -0700

    Added sinewave animation fully

--- app/src/main/java/com/axiomofchoice/whitenoise/GameSurfaceView.java
+++ app/src/main/java/com/axiomofchoice/whitenoise/GameSurfaceView.java
@@ -7,9 +7,11 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class GameSurfaceView extends SurfaceView implements Runnable {
     private boolean isRunning = false;
@@ -24,6 +26,14 @@ public class GameSurfaceView extends SurfaceView implements Runnable {
     int rVal = 0;
     int gVal = 0;
     int bVal = 0;
+    int pt = 100; // number of points to generate shapes - e.g. sample points on image
+    int[] X = new int[pt]; // container for the sample points - X
+    int[] Y = new int[pt]; // container for the sample points - Y
+    int max = 10; // max value for the sawtooth wave modulator
+    int[] st = new int[max*2]; // contained for the sawtooth modulator
+    int modulator_pointer = 0; // modulator loop counter for draw loop
+
+
 
     private Bitmap mlk;
     private Wave testWave = new Wave(150,150,0,0,0,0,0,0,0,0,0);
@@ -112,9 +122,9 @@ public class GameSurfaceView extends SurfaceView implements Runnable {
         // just for testing
         int width;
         int height;
-        float r; // red
-        float g; // green
-        float b; // blue
+        int r; // red
+        int g; // green
+        int b; // blue
         float freq; // "frequency" of sinewave
         float amplitude; // "amplitude" of sinewave
         float step; // kind of like the "sampling" frequency
@@ -125,18 +135,18 @@ public class GameSurfaceView extends SurfaceView implements Runnable {
         Paint q = null;
 
         // Constructor
-        Wave(int _x, int _y, float _r, float _g, float _b, float _f, float _a, float _s, int _shift, float _rotate, float _period) {
+        Wave(int _x, int _y, int _r, int _g, int _b, float _f, float _a, float _s, int _shift, float _rotate, float _period) {
             x = _x; y = _y; r = _r; g = _g; b = _b;
             freq = _f; amplitude = _a; step = _s;
             shift = _shift; rotate = _rotate; period = _period;
             // Set some defaults for testing...
-            shift = 1;
-            freq = 5;
-            amplitude = 20;
-            period = 2*Math.PI;
-            step = 2;
+            //shift = 1;
+            //freq = 5;
+            //amplitude = 20;
+            //period = 2*Math.PI;
+            //step = 2;
             q = new Paint();
-            q.setColor(Color.rgb(rVal,  gVal, bVal));
+            q.setColor(Color.rgb(r, g, b));
             q.setStrokeWidth(2);
             q.setStyle(Paint.Style.STROKE);
         }
@@ -161,8 +171,65 @@ public class GameSurfaceView extends SurfaceView implements Runnable {
 
     }
 
-    protected void render(Canvas canvas) {
 
+    protected void rand_points(int xl, int yl, int xr, int yr) {
+        Log.d("wn","Screen-width"+String.valueOf(mlk.getWidth()));
+        for (int i=0; i<pt; i++) {
+            if(xl==-1 && xr==-1){
+                X[i] = (int) (mlk.getWidth() * Math.random());
+            } else {
+                // int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
+                X[i] = ThreadLocalRandom.current().nextInt(xl,xr+1);
+            }
+            if(yl==-1 && yr==-1) {
+                Y[i] = (int) (mlk.getHeight() * Math.random());
+            } else {
+                Y[i] = ThreadLocalRandom.current().nextInt(yl, yr + 1);
+            }
+        }
+    }
+
+    protected void sawtooth(){
+        for(int i = 0; i<max; i++){
+            st[i] = i+1;
+        }
+        for(int i = max; i<2*max; i++){
+            st[i] = 2*max-i;
+        }
+    }
+
+    protected void ptz(float f, float a, float s, int shift, float rotate, boolean bg, String shape){
+        if (bg) cv.drawColor(Color.BLACK);
+
+        for (int i=0; i<pt; i++) {
+            int x = X[i];
+            int y = Y[i];
+            //Log.d("wn", "x:" + String.valueOf(x) + "y:" + String.valueOf(y));
+            // get colors
+            int pixel = mlk.getPixel(x,y);
+
+            //then do what you want with the pixel data, e.g
+            int r = Color.red(pixel);
+            int g = Color.green(pixel);
+            int b = Color.blue(pixel);
+            //Log.d("wn","Green pixel:" + String.valueOf(g));
+            Wave myWave = new Wave(150,150,0,0,0,0,0,0,0,0,0);
+            if(r>0){
+                    float period = (float)(2*Math.PI);
+                    myWave = new Wave(x,y,r,g,b,f,a,s,0,rotate,period);
+                }
+            myWave.display(cv);
+
+            }
+    }
+
+    boolean black_background = false;
+
+    protected void render(Canvas canvas) {
+        step = 2;
+        int shift = 3;
+        float rotate = 0;
+        float freq = 5;
         // Set background color
         //canvas.drawColor(Color.BLACK);
         // Add the MLK image
@@ -174,15 +241,23 @@ public class GameSurfaceView extends SurfaceView implements Runnable {
         // Add the frame per second and touch co-ords labels
         canvas.drawText("FPS:" + fps, 20, 20, p);
         canvas.drawText("Touch:" + xPos + ":" + yPos, 20, 50, p);
-        //step = (float) (Math.random() - 1.5*Math.random());
+        step = (float) (Math.random() - 1.5*Math.random());
+        int amplitude = 2*st[modulator_pointer];
+        //int amplitude = 2;
+        ptz(freq,amplitude,step,shift,rotate,black_background,"sinewave");
+        modulator_pointer++;
+        if (modulator_pointer==st.length-1){
+            modulator_pointer = 0;}
         //w.step = step;
-        testWave.q.setColor(Color.rgb(rVal,  gVal, bVal));
-        testWave.display(canvas);
+        //testWave.q.setColor(Color.rgb(rVal,  gVal, bVal));
+        //testWave.display(canvas);
     }
 
     @Override
     public void run() {
-
+        //rand_points(-1,-1,-1,-1);
+        rand_points(160,147,517,587);
+        sawtooth();
         while(isRunning) {
             // We need to make sure that the surface is ready
             if (! holder.getSurface().isValid()) {
